d51ac4d88bb8a67a21aa0512902ef33bd56ea346
lefasom <lefasom>
2022-12-17 22:03:07 -0300

    acomodo el margin de la img principal

--- pages/index.js
+++ pages/index.js
@@ -50,7 +50,7 @@ export default function Home() {
     {`
         .fotoPrincipal img {
           width: 100%;
-          height: 100vh;
+          height: 120vh;
         }
     `}  
     </style>   
